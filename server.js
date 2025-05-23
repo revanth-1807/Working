26e2ffac60f0821fbd77188bc6b7011095d239db
revanth-1807 <revanth1807r@gmail.com>
2025-05-23 15:14:57 +0530

    server.js

--- server.js
+++ server.js
@@ -43,7 +43,9 @@ m.connect(process.env.M)
 .catch((err)=>{
     console.log('Not connected')
 })
-
+app.get('/get',(req,res)=>{
+  res.render('login');
+})
 app.get('/register', (req, res) => {
     res.render('register');
   });
